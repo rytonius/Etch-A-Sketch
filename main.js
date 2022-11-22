588f22b7f3edbeec1f7ba6a3705b3c24adcfe579
rytonius <ryan3rdeye@gmail.com>
2022-11-22 13:07:21 -0900

    removed grid total

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 function CreateGrid() {
     const Sketch = document.querySelector('Right');
-    let rowCount = 16;
-    let gridtotal = rowCount * rowCount;
+    let gridsize = 16;
     let count = 0;
 
     Sketch.style.display = 'grid';
-    Sketch.style.gridTemplateRows = `repeat(${rowCount}, 1fr)`;
-    Sketch.style.gridTemplateColumns = `repeat(${rowCount}, 1fr)`;
+    Sketch.style.gridTemplateRows = `repeat(${gridsize}, 1fr)`;
+    Sketch.style.gridTemplateColumns = `repeat(${gridsize}, 1fr)`;
 
     let row = 1;
     let column = 1;
-    for (let i = 1; i <= gridtotal; i++) {
+    for (let i = 1; i <= gridsize * gridsize; i++) {
 
         console.log("grid section made, count: " + count);
         count += 1;
@@ -21,7 +20,7 @@ function CreateGrid() {
         gridunit.style.gridRow = row;
         gridunit.style.gridColumn = column;
         column += 1;
-        if (column === rowCount + 1 ) {
+        if (column === gridsize + 1 ) {
             row += 1;
             column = 1;
         }
