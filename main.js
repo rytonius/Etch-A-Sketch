81e1014925f4e5022629996d81b309d88d78dfb3
rytonius <ryan3rdeye@gmail.com>
2023-01-09 00:07:54 -0900

    Etch-a-sketch

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-function CreateGrid() {
+function CreateGrid(sizeofgrid) {
+    RemoveGrid();
+
     const Sketch = document.querySelector('Right');
-    let gridsize = 16;
+    let gridsize = sizeofgrid;
     let count = 0;
 
     Sketch.style.display = 'grid';
@@ -13,14 +15,22 @@ function CreateGrid() {
 
         console.log("grid section made, count: " + count);
         count += 1;
+
         var gridunit = document.createElement('gu');
         gridunit.style.border = '1px solid black';
         gridunit.id = count;
-        gridunit.textContent = count;
+        //gridunit.textContent = count;
         gridunit.style.gridRow = row;
         gridunit.style.gridColumn = column;
+        gridunit.addEventListener("mouseover", (event) => {
+            event.target.style.backgroundColor = "yellow";
+        })
+
+        gridunit.addEventListener("mouseleave", (event) => {
+            event.target.style.backgroundColor = "grey";
+        })
         column += 1;
-        if (column === gridsize + 1 ) {
+        if (column === gridsize + 1) {
             row += 1;
             column = 1;
         }
@@ -30,4 +40,18 @@ function CreateGrid() {
     }
 }
 
-CreateGrid();
+function RemoveGrid() {
+    var gridunits = document.getElementsByTagName("gu");
+    var TotalLength = gridunits.length
+    if (gridunits.length > 0) {
+        console.log(gridunits.length)
+        for (var i = 0; i < TotalLength; i += 1) {
+            console.log(i)
+            console.log(gridunits[0].id)
+            gridunits[0].remove();
+        }
+    }
+
+}
+
+CreateGrid(16);
